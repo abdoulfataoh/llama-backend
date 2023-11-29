e52fefe50749360194f73c535d21a6d09920aec4
abdoulfataoh <abdoulfataoh@gmail.com>
2023-11-29 14:47:01 +0000

    add redis settings

--- app/settings.py
+++ app/settings.py
@@ -11,3 +11,8 @@ env.read_env()
 
 # llama
 LLAMA_MODEL_NAME = env.str('LLAMA_MODEL_NAME', '')
+
+
+# Redis
+REDIS_DATABASE_URL = env.url('REDIS_DATABASE_URL', 'http://localhost')
+REDIS_DATABASE_PORT = env.str('REDIS_DATABASE_PORT', '6379')
